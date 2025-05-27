3f2b9e71635c08feb4f6ab2ff994b508e7cdaa82
João Pedro <joaojosafa11@gmail.com>
2025-05-27 18:16:05 -0300

    removendo arquivos

--- config/conn.php
+++ config/conn.php
@@ -1,13 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "projetoinovacao";
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Create connection
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
