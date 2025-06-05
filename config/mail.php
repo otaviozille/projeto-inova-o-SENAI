76b023d73dd344867f9973deed2f1d507c1e1940
Otavio Zille <144745556+otaviozille@users.noreply.github.com>
2025-06-05 04:45:45 -0300

    Add files via upload

---
+++ config/mail.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'host' => 'smtp.gmail.com',
+  'username' => 'test.petrobras@gmail.com', 
+  'password' => 'yepz axut xqzu lvwl', 
+  'port' => 587,
+  'from_email' => 'test.petrobras@gmail.com',
+  'from_name' => 'Sistema Petrobras'
+];
